8beb648b76d3b6ce23c9084adf5a952d62baacbe
Ramond Falcão <ramondfalcao@gmail.com>
2022-09-14 19:46:20 -0300

    feat: :white_check_mark: Implementando test para readOne da Camada Model

--- src/tests/unit/models/seu-teste-model.test.ts
+++ src/tests/unit/models/seu-teste-model.test.ts
@@ -9,9 +9,8 @@ describe('Car Model', () => {
   const carModel = new CarModel();
 
   before(async () => {
-    sinon
-      .stub(Model, 'create')
-      .resolves(carMockWithId);
+    sinon.stub(Model, 'create').resolves(carMockWithId);
+    sinon.stub(Model, 'findOne').resolves(carMockWithId);
   });
 
   after(()=>{
@@ -23,4 +22,19 @@ describe('Car Model', () => {
       const newCar = await carModel.create(carMock)
       expect(newCar).to.be.deep.equal(carMockWithId)
     })
+
+  describe('searching a car', () => {
+    it('successfully found', async () => {
+      const framesFound = await carModel.readOne('62cf1fc6498565d94eba52cd');
+      expect(framesFound).to.be.deep.equal(carMockWithId);
+    });
+
+    it('_id not found', async () => {
+      try {
+        await carModel.readOne('XABLAU');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId');
+      }
+    });
+  });
 });

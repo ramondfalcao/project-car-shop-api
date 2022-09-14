cf50b75d236e0914ca7ef551320bc43db16bb54d
Ramond Falcão <ramondfalcao@gmail.com>
2022-09-14 19:54:18 -0300

    feat: :white_check_mark: Implementando teste para ReadOne da Camada Controller

--- src/tests/unit/controllers/seu-test-controller.test.ts
+++ src/tests/unit/controllers/seu-test-controller.test.ts
@@ -18,6 +18,7 @@ describe('Car Controller', () => {
 
   before(async () => {
     sinon.stub(carService, 'create').resolves(carMock);
+    sinon.stub(carService, 'readOne').resolves(carMock);
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
   });
@@ -32,6 +33,16 @@ describe('Car Controller', () => {
       await carController.create(req, res);
 
       expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+    });
+  });
+
+  describe('ReadOne Car', () => {
+    it('Success', async () => {
+      req.params = { id: carMockWithId._id };
+      await carController.readOne(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
       expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
     });
   });

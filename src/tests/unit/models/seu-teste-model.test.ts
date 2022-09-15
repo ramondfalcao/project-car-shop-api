97500fbbd8fcbb4c7e94673066d6ed6bb8c8a86a
Ramond Falcão <ramondfalcao@gmail.com>
2022-09-15 12:10:31 -0300

    feat: :white_check_mark: Implementando Teste para o Delete da Camada Model

--- src/tests/unit/models/seu-teste-model.test.ts
+++ src/tests/unit/models/seu-teste-model.test.ts
@@ -12,22 +12,24 @@ describe('Car Model', () => {
     sinon.stub(Model, 'create').resolves(carMockWithId);
     sinon.stub(Model, 'findOne').resolves(carMockWithId);
     sinon.stub(Model, 'findOneAndUpdate').resolves(carMockWithId);
+    sinon.stub(Model, 'findOneAndDelete');
   });
 
   after(()=>{
     sinon.restore();
   })
 
-  describe('creating a car', async () => {});
+  describe('Creating a car', async () => {
     it('sucessfully created', async () => {
       const newCar = await carModel.create(carMock)
       expect(newCar).to.be.deep.equal(carMockWithId)
     })
+  });
 
   describe('searching a car', () => {
     it('successfully found', async () => {
-      const framesFound = await carModel.readOne('62cf1fc6498565d94eba52cd');
-      expect(framesFound).to.be.deep.equal(carMockWithId);
+      const carFound = await carModel.readOne('62cf1fc6498565d94eba52cd');
+      expect(carFound).to.be.deep.equal(carMockWithId);
     });
 
     it('_id not found', async () => {
@@ -40,14 +42,29 @@ describe('Car Model', () => {
   });
 
   describe('Updating a car', () => {
-    it('successfully found', async () => {
-      const framesFound = await carModel.update('62cf1fc6498565d94eba52cd', carMock);
-      expect(framesFound).to.be.deep.equal(carMockWithId);
+    it('Successfully Updating', async () => {
+      const carUpdating = await carModel.update('62cf1fc6498565d94eba52cd', carMock);
+      expect(carUpdating).to.be.deep.equal(carMockWithId);
     });
 
     it('_id not found', async () => {
       try {
         await carModel.update('XABLAU', carMock);
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId');
+      }
+    });
+  });
+
+  describe('Deleting a car', () => {
+    it('Successfully Deleting', async () => {
+      const carDeleting = await carModel.delete('62cf1fc6498565d94eba52cd');
+      expect(carDeleting);
+    });
+
+    it('_id not found', async () => {
+      try {
+        await carModel.delete('XABLAU');
       } catch (error: any) {
         expect(error.message).to.be.eq('InvalidMongoId');
       }
